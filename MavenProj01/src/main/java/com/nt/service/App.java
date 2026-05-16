70e09572d1c9832c21df28b6eb2e4036b4d78a26
sanket <mishra.sanket501@gmail.com>
2026-05-16 09:32:55 +0530

    TL added sum() in App.java

--- MavenProj01/src/main/java/com/nt/service/App.java
+++ MavenProj01/src/main/java/com/nt/service/App.java
@@ -4,7 +4,11 @@ package com.nt.service;
  * Hello world!
  */
 public class App {
+	public int sum(int x, int y) {
+		return x+y;
+	}
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        App app=new App();
+        System.out.println("sum is :: "+app.sum(10, 20));
     }
 }
