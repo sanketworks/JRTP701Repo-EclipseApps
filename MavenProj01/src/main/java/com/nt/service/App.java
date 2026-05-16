f6f9320c9b4b2c2ecfb11e22864de80c78b4e7e3
DELL <mishra.sanket108@gmail.com>
2026-05-16 10:20:36 +0530

    machine 2 added div() in App.java

--- MavenProj01/src/main/java/com/nt/service/App.java
+++ MavenProj01/src/main/java/com/nt/service/App.java
@@ -13,6 +13,9 @@ public class App {
 	public int mul(int x,int y) {
 		return x*y;
 	}
+	public int div(int x, int y) {
+		return x/y;
+	}
     public static void main(String[] args) {
         App app=new App();
         System.out.println("sum is :: "+app.sum(10, 20));
