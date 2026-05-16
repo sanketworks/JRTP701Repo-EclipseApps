421183c8bd42602ae9daffb64bf0eb3de0508e48
sanket <mishra.sanket501@gmail.com>
2026-05-16 19:30:05 +0530

    bug 123 is fixed

--- MavenProj01/src/main/java/com/nt/service/App.java
+++ MavenProj01/src/main/java/com/nt/service/App.java
@@ -18,9 +18,14 @@ public class App {
 		return x/y;
 	}
 	
+
 	public String reverse(String name) {
 		StringBuilder sb=new StringBuilder(name);
 		return sb.reverse().toString();
+	}
+	public int findNameLength(String name) {
+		System.out.println("App.findNameLength() bug 123 fixing is going on");
+		return name.length();
 	}
     public static void main(String[] args) {
         App app=new App();
